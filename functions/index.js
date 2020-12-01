efd922fd197385841f56f5a6f830e4b7155f9c9b
viet.tranhoang <viet.tranhoang@powergatesoftware.com>
2020-12-01 10:30:09 +0700

    init view

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// static files
+app.use(express.static('public'));
+app.use('/css',express.static(__dirname+'public/css'));
+app.use('/js',express.static(__dirname+'public/js'));
+app.use('/images',express.static(__dirname+'public/images'));
+app.use('/fonts',express.static(__dirname+'public/fonts'));
+
+// 
+app.set('view engine','ejs')
+
+// navigatetion
+app.get('',(req,res)=>{
+    res.render('index')
+})
+exports.pizza   = functions.https.onRequest(app);
